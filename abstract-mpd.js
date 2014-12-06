c4a31eef73979a01fbddb746fdf4e14df85d89c1
MerynT <merijntieltjes@hotmail.com>
2014-12-06 22:08:06 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -1,46 +1,45 @@
-// https://helloacm.com
-// 09-Feb-2013
+// Load the TCP Library
+net = require('net');
  
-var sys = require('sys');
-var net = require('net');
-var sockets = [];
+// Keep track of the chat clients
+var clients = [];
  
-var svr = net.createServer(function(sock) {
-    sys.puts('Connected: ' + sock.remoteAddress + ':' + sock.remotePort); 
-    sock.write('Hello ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
-    sockets.push(sock);
+// Start a TCP Server
+net.createServer(function (socket) {
  
-    sock.on('data', function(data) {  // client writes message
-        if (data == 'exit\n') {
-            sys.puts('exit command received: ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
-            sock.destroy();
-            var idx = sockets.indexOf(sock);
-            if (idx != -1) {
-                delete sockets[idx];
-            }
-            return;
-        }
-        var len = sockets.length;
-        for (var i = 0; i < len; i ++) { // broad cast
-            if (sockets[i] != sock) {
-                if (sockets[i]) {
-                    sockets[i].write(sock.remoteAddress + ':' + sock.remotePort + ':' + data);
-                }
-            }
-        }
+  // Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+ 
+  // Put this new client in the list
+  clients.push(socket);
+ 
+  // Send a nice welcome message and announce
+  socket.write("Welcome " + socket.name + "\n");
+  broadcast(socket.name + " joined the chat\n", socket);
+ 
+  // Handle incoming messages from clients.
+  socket.on('data', function (data) {
+    broadcast(socket.name + "> " + data, socket);
+  });
+ 
+  // Remove the client from the list when it leaves
+  socket.on('end', function () {
+    clients.splice(clients.indexOf(socket), 1);
+    broadcast(socket.name + " left the chat.\n");
+  });
+  
+  // Send a message to all clients
+  function broadcast(message, sender) {
+    clients.forEach(function (client) {
+      // Don't want to send it to sender
+      if (client === sender) return;
+      client.write(message);
     });
+    // Log it to the server output too
+    process.stdout.write(message)
+  }
  
-    sock.on('end', function() { // client disconnects
-        sys.puts('Disconnected: ' + data + data.remoteAddress + ':' + data.remotePort + '\n');
-        var idx = sockets.indexOf(sock);
-        if (idx != -1) {
-            delete sockets[idx];
-        }
-    });
-});
+}).listen(6601);
  
-var svraddr = '0.0.0.0';
-var svrport = 8080;
- 
-svr.listen(svrport, svraddr);
-sys.puts('Server Created at ' + svraddr + ':' + svrport + '\n');
+// Put a friendly message on the terminal of the server.
+console.log("Chat server running at port 6601\n");
