84a9e510d00788959489bf0a4bf46cea48ea8dbd
MerynT <merijntieltjes@hotmail.com>
2014-12-06 21:58:01 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -1,11 +1,11 @@
 var net = require('net');
 
-net.createServer(function(socket)) {
+net.createServer(function(socket) {
     
     socket.on('data', function(data) {
         socket.write(data);
     });
     
-}.listen(6601);
+}).listen(6601);
 
 console.log("MPD Abstraction layer listening on port 6601....");
