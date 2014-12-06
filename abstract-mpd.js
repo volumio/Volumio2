861a6e64746420d739469c121fbaa2f23a48382c
MerynT <merijntieltjes@hotmail.com>
2014-12-06 22:01:06 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -1,11 +1,46 @@
+// https://helloacm.com
+// 09-Feb-2013
+ 
+var sys = require('sys');
 var net = require('net');
-
-net.createServer(function(socket) {
-    
-    socket.on('data', function(data) {
-        socket.write(data);
+var sockets = [];
+ 
+var svr = net.createServer(function(sock) {
+    sys.puts('Connected: ' + sock.remoteAddress + ':' + sock.remotePort); 
+    sock.write('Hello ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
+    sockets.push(sock);
+ 
+    sock.on('data', function(data) {  // client writes message
+        if (data == 'exit\n') {
+            sys.puts('exit command received: ' + sock.remoteAddress + ':' + sock.remotePort + '\n');
+            sock.destroy();
+            var idx = sockets.indexOf(sock);
+            if (idx != -1) {
+                delete sockets[idx];
+            }
+            return;
+        }
+        var len = sockets.length;
+        for (var i = 0; i < len; i ++) { // broad cast
+            if (sockets[i] != sock) {
+                if (sockets[i]) {
+                    sockets[i].write(sock.remoteAddress + ':' + sock.remotePort + ':' + data);
+                }
+            }
+        }
     });
-    
-}).listen(6601);
-
-console.log("MPD Abstraction layer listening on port 6601....");
+ 
+    sock.on('end', function() { // client disconnects
+        sys.puts('Disconnected: ' + data + data.remoteAddress + ':' + data.remotePort + '\n');
+        var idx = sockets.indexOf(sock);
+        if (idx != -1) {
+            delete sockets[idx];
+        }
+    });
+});
+ 
+var svraddr = '127.0.0.1';
+var svrport = 8080;
+ 
+svr.listen(svrport, svraddr);
+sys.puts('Server Created at ' + svraddr + ':' + svrport + '\n');
