68b82d9c52a0d099c8d844ec75f7569db621bc2f
MerynT <merijntieltjes@hotmail.com>
2014-12-06 21:56:23 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -1,30 +1,11 @@
-var net = require('net');
+var net = require(‘net’);
 
-var HOST = 'localhost';
-var PORT = 6601;
-
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
+net.createServer(function(socket)) {
     
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    
-    // Add a 'data' event handler to this instance of socket
-    sock.on('data', function(data) {
-        
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
-        
+    socket.on('data', function(data) {
+        socket.write(data);
     });
     
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-    });
-    
-}).listen(PORT, HOST);
+}.listen(6601);
 
-console.log('Server listening on ' + HOST +':'+ PORT);
+console.log("MPD Abstraction layer listening on port 6601....");
