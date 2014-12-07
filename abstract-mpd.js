85fef9db6252d9d65e563585fadcb74f021fe22a
MerynT <merijntieltjes@hotmail.com>
2014-12-07 23:36:57 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -13,7 +13,10 @@ var protocolServer = net.createServer(function(socket) {
     socket.setEncoding('utf8');
     
     // add client to list
-    clients.push(socket);
+    socket.on('connected', function(socket) {
+        sys.puts("New client connected: " + socket.remoteAddress +':'+ socket.remotePort);
+        clients.push(socket);
+    });
     
     // MPD welcome command
     socket.write("OK MPD -version-\n");
@@ -21,7 +24,8 @@ var protocolServer = net.createServer(function(socket) {
     // handle errors in handleError function
     socket.on('error', handleError);
     
-    
+    // on incoming message
+    socket.on('data', handleMessage);
 
     function handleError(err) {
       log.error("socket error:", err.stack);
@@ -44,30 +48,37 @@ protocolServer.on('error', function(err) {
     }
 });
 
-// on incoming message
-protocolServer.on('data', function(message) {
-        // log data (only for debugging)
-        sys.puts("received: " + message);
-        // cast message to string
-        var data = message.toString();
-
-        // read command
-        if(data.startsWith('play')) {
-            sys.puts("play command received");
-            socket.write("OK\n");
-        } else if(data.startsWith("stop")) {
-            sys.puts("stop command received");
-            socket.write("OK\n");
-        } else {
-            sys.puts("command not recognized: " + data);
-            socket.write("ACK\n");
-        }        
-    });
-
 protocolServer.listen(mpdPort, mpdHost, function() {
     sys.puts("Abstract MPD layer listening at: " +
     mpdHost + ":" + mpdPort);
 });
+
+function  handleMessage(data){
+    // log data (only for debugging)
+    sys.puts("received: " + data);
+    // cast message to string
+    var message = data.toString();
+
+    // read command
+    if(message.startsWith('play')) {
+        // play command
+        sys.puts("play command received");
+        sendSingleCommandToCore("play");
+        socket.write("OK\n");
+    } else if(message.startsWith("stop")) {
+        // stop command
+        sys.puts("stop command received");
+        // TODO send stop to core
+        socket.write("OK\n");
+    } else {
+        sys.puts("command not recognized: " + message);
+        socket.write("ACK\n");
+    }        
+}
+
+function sendSingleCommandToCore(command) {
+    // Foward the command to the Core (no editing needed)
+}
     
 String.prototype.startsWith = function (str){
     return this.slice(0, str.length) == str;
