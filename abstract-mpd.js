db8ee1d70350f06a3900365965bf75053814f198
MerynT <merijntieltjes@hotmail.com>
2014-12-07 23:02:07 +0100

    Update abstract-mpd.js

--- abstract-mpd.js
+++ abstract-mpd.js
@@ -1,17 +1,27 @@
 var net = require('net');
 var sys = require('sys');
 
+// server settings
 var mpdPort = 6601;
 var mpdHost = '0.0.0.0';
 
+// keep track of connected clients (for broadcasts)
 var clients = [];
 
+// create server
 var protocolServer = net.createServer(function(socket) {
     socket.setEncoding('utf8');
+    
+    // add client to list
     clients.push(socket);
+    
+    // MPD welcome command
     socket.write("OK MPD -version-\n");
     
+    // handle errors in handleError function
     socket.on('error', handleError);
+    
+    
 
     function handleError(err) {
       log.error("socket error:", err.stack);
@@ -22,18 +32,26 @@ var protocolServer = net.createServer(function(socket) {
     function handleError(err) {
         log.error("socket error:", err.stack);
         socket.destroy();
-        cleanup();
     }
+});
 
-    function cleanup() {
-        protocol.close();
-    }    
+protocolServer.on('error', function(err) {
+    if (err.code === 'EADDRINUSE') {
+      sys.puts("Failed to bind MPD protocol to port " + mpdPort +
+        ": Address in use.");
+    } else {
+      throw err;
+    }
+});
 
-
-    socket.on('data', function(message) {
+// on incoming message
+protocolServer.on('data', function(message) {
+        // log data (only for debugging)
         sys.puts("received: " + message);
+        // cast message to string
         var data = message.toString();
 
+        // read command
         if(data.startsWith('play')) {
             sys.puts("play command received");
             socket.write("OK\n");
@@ -45,17 +63,6 @@ var protocolServer = net.createServer(function(socket) {
             socket.write("ACK\n");
         }        
     });
-
-});
-
-protocolServer.on('error', function(err) {
-    if (err.code === 'EADDRINUSE') {
-      sys.puts("Failed to bind MPD protocol to port " + mpdPort +
-        ": Address in use.");
-    } else {
-      throw err;
-    }
-});
 
 protocolServer.listen(mpdPort, mpdHost, function() {
     sys.puts("Abstract MPD layer listening at: " +
