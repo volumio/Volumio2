cd868368d8257c9891787de92987ed0f22ca4861
ning-yu <ningyu@lightflo.ws>
2015-02-01 15:53:05 +0000

    Removing node_modules from repo
    - Add entry to ignore node_modules folder in .gitignore
    - Modified interface-webUI so that command responses only get sent to requestor instead of broadcast

--- controllers/interface-webUI.js
+++ controllers/interface-webUI.js
@@ -14,24 +14,36 @@ function InterfaceWebUI (server, commandRouter) {
 
 		// Listen for the various types of client events -----------------------------
 		connWebSocket.on('volumioGetState', function() {
+			_thisConnWebSocket = this;
+
 			logStart('volumioGetState')
 				.then(commandRouter.volumioGetState.bind(commandRouter))
-				.then(_this.volumioPushState.bind(_this))
+				.then(function (state) {
+					return _this.volumioPushState.call(_this, state, _thisConnWebSocket);
+
+				})
 				.catch(console.log)
 				.done(logDone);
 
 		});
 
 		connWebSocket.on('volumioGetQueue', function() {
+			_thisConnWebSocket = this;
+
 			logStart('volumioGetQueue')
 				.then(commandRouter.volumioGetQueue.bind(commandRouter))
-				.then(_this.volumioPushQueue.bind(_this))
+				.then(function (queue) {
+					return _this.volumioPushQueue.call(_this, queue, _thisConnWebSocket);
+
+				})
 				.catch(console.log)
 				.done(logDone);
 
 		});
 
 		connWebSocket.on('volumioPlay', function() {
+			_thisConnWebSocket = this;
+
 			logStart('volumioPlay')
 				.then(commandRouter.volumioPlay.bind(commandRouter))
 				.catch(console.log)
@@ -56,24 +68,36 @@ InterfaceWebUI.prototype.printConsoleMessage = function (message) {
 }
 
 // Receive player queue updates from commandRouter and broadcast to all connected clients
-InterfaceWebUI.prototype.volumioPushQueue = function (queue) {
+InterfaceWebUI.prototype.volumioPushQueue = function (queue, connWebSocket) {
 
 	console.log('InterfaceWebUI::volumioPushQueue');
 	var _this = this;
 
-	// Push the updated queue to all clients
-	return libQ.invoke(_this.libSocketIO, 'emit', 'volumioPushQueue', queue);
+	if (connWebSocket) {
+		return libQ.invoke(connWebSocket, 'emit', 'volumioPushQueue', queue);
+
+	} else {
+		// Push the updated queue to all clients
+		return libQ.invoke(_this.libSocketIO, 'emit', 'volumioPushQueue', queue);
+
+	}
 
 }
 
 // Receive player state updates from commandRouter and broadcast to all connected clients
-InterfaceWebUI.prototype.volumioPushState = function (state) {
+InterfaceWebUI.prototype.volumioPushState = function (state, connWebSocket) {
 
 	console.log('InterfaceWebUI::volumioPushState');
 	var _this = this;
 
-	// Push the updated state to all clients
-	return libQ.invoke(_this.libSocketIO, 'emit', 'volumioPushState', state);
+	if (connWebSocket) {
+		return libQ.invoke(connWebSocket, 'emit', 'volumioPushState', state);
+
+	} else {
+		// Push the updated state to all clients
+		return libQ.invoke(_this.libSocketIO, 'emit', 'volumioPushState', state);
+
+	}
 
 }
 
