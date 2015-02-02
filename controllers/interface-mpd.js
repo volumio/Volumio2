4b757a9daa30a7876570c9a57bb6868418d0b243
MerynT <merijntieltjes@hotmail.com>
2015-02-03 00:30:03 +0100

    WIP

--- controllers/interface-mpd.js
+++ controllers/interface-mpd.js
@@ -1,6 +1,6 @@
 // This module work as interface for all the third parties client wich want to talk with an MPD server.
 // This module get all the MPD client request but they will be handled from Volumio Core Module
-
+var libQ = require('q');
 var net = require('net');
 var sys = require('sys');
 // server settings
