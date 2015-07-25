fa3a43f05edfb4b0e981fd86ad2d2939ceb253e8
Your Name <info@volumio.org>
2015-07-25 18:43:36 +0000

    Further Info on example Plugin

--- app/plugins/music_services/example_plugin/index.js
+++ app/plugins/music_services/example_plugin/index.js
@@ -11,69 +11,82 @@ function ExamplePlugin(commandRouter) {
  * The Core controller checks if the method is defined and executes it on startup if it exists.
  */
 ExamplePlugin.prototype.onVolumioStart = function() {
+    var self = this;
 	//Perform startup tasks here
 }
 
 ExamplePlugin.prototype.onStart = function() {
+    var self = this;
     //Perform startup tasks here
 }
 
 ExamplePlugin.prototype.onStop = function() {
+    var self = this;
     //Perform startup tasks here
 }
 
 ExamplePlugin.prototype.onRestart = function() {
+    var self = this;
     //Perform startup tasks here
 }
 
 ExamplePlugin.prototype.onInstall = function()
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.onUninstall = function()
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.getUIConfig = function()
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.setUIConfig = function(data)
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.getConf = function(varName)
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.setConf = function(varName, varValue)
 {
+    var self = this;
     //Perform your installation tasks here
 }
-
 
 //Optional functions exposed for making development easier and more clear
 ExamplePlugin.prototype.getSystemConf = function(pluginName,varName)
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.setSystemConf = function(pluginName,varName)
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.getAdditionalConf = function()
 {
+    var self = this;
     //Perform your installation tasks here
 }
 
 ExamplePlugin.prototype.setAdditionalConf = function()
 {
+    var self = this;
     //Perform your installation tasks here
 }
