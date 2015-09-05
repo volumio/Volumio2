d36c703776283463ff04733a39ef0f1c813282c4
volumio <info@volumio.org>
2015-09-05 11:17:22 +0000

    unincluded library and playlistfs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('newrelic');
 var expressInstance = require('./http/index.js');
 var expressApp = expressInstance.app;
+
 // Using port 3000 for the debug interface
 expressApp.set('port', 3000);
 
