d7e89168d51ea04a329933796c2fbc5d9fd49021
Massimiliano Fanciulli <fanciulli@gmail.com>
2015-09-13 15:43:36 +0200

    Committing test file

--- app/testMusicLibrary.js
+++ app/testMusicLibrary.js
@@ -14,9 +14,31 @@ socket.on('pushBrowseSources',function(data)
 });
 */
 console.log("GET BrowseLibrary\n\n");
-socket.emit('BrowseLibrary',{uri:'music-library/USB/Audioslave/Out of Exile'});
+//socket.emit('getMultiroom',{uri:'music-library/USB/Audioslave/Out of Exile'});
 
-socket.on('pushBrowseLibrary',function(data)
+var url='http://192.168.10.135:3000';
+/*setTimeout(function()
+{
+    socket.emit('setMultiroom',{ip:'http://192.168.10.135:3000',set:'server',volume:80});
+
+    setTimeout(function(){
+        socket.emit('getMultiroom',{uri:'music-library/USB/Audioslave/Out of Exile'});
+    },3000);
+},3000);*/
+
+setTimeout(function()
+{
+    socket.emit('setMultiroom',{ip:'http://192.168.10.135:3000',set:'server',volume:80});
+
+    setTimeout(function(){
+            socket.emit('setMultiroom',{ip:'http://192.168.10.115:3000',set:'client',volume:80});
+
+
+        },3000);
+
+},3000);
+
+socket.on('pushMultiroom',function(data)
 {
     console.log(JSON.stringify(data));
 });
