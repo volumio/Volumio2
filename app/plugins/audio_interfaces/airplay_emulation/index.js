92068aec6f64c0037c5b6fcacc8668a23ff23089
volumio <info@volumio.org>
2015-10-03 23:36:28 +0000

    Airplay

---
+++ app/plugins/audio_interfaces/airplay_emulation/index.js
@@ -0,0 +1,119 @@
+var libQ = require('kew');
+var libNet = require('net');
+var libFast = require('fast.js');
+var libLevel = require('level');
+var fs=require('fs-extra');
+var exec = require('child_process').exec;
+
+// Define the UpnpInterface class
+module.exports = AirPlayInterface;
+
+
+function AirPlayInterface(context) {
+	var self = this;
+    // Save a reference to the parent commandRouter
+    self.context=context;
+    self.commandRouter = self.context.coreCommand;
+
+}
+
+AirPlayInterface.prototype.onVolumioStart = function() {
+    var self = this;
+
+    self.context.coreCommand.pushConsoleMessage('[' + Date.now() + '] Starting Shairport Sync');
+    self.startShairportSync();
+
+}
+
+AirPlayInterface.prototype.onStart = function() {
+    var self = this;
+
+}
+
+AirPlayInterface.prototype.onStop = function() {
+    var self = this;
+    //Perform startup tasks here
+}
+
+AirPlayInterface.prototype.onRestart = function() {
+    var self = this;
+    //Perform startup tasks here
+}
+
+AirPlayInterface.prototype.onInstall = function()
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.onUninstall = function()
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.getUIConfig = function()
+{
+    var self = this;
+
+
+}
+
+AirPlayInterface.prototype.setUIConfig = function(data)
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.getConf = function(varName)
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.setConf = function(varName, varValue)
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+//Optional functions exposed for making development easier and more clear
+AirPlayInterface.prototype.getSystemConf = function(pluginName,varName)
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.setSystemConf = function(pluginName,varName)
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.getAdditionalConf = function()
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.setAdditionalConf = function()
+{
+    var self = this;
+    //Perform your installation tasks here
+}
+
+AirPlayInterface.prototype.startShairportSync = function() {
+    var self = this;
+
+    var systemController = self.commandRouter.pluginManager.getPlugin('system_controller', 'system');
+    var name = systemController.getConf('playerName');
+
+    exec("shairport-sync -a '"+ name +"' hw:1,0", function (error, stdout, stderr) {
+        if (error !== null) {
+            self.context.coreCommand.pushConsoleMessage('[' + Date.now() + '] Shairport-sync error: ' + error);
+        }
+        else {
+            self.context.coreCommand.pushConsoleMessage('[' + Date.now() + '] Shairport-Sync Started');
+        }
+    });
+}
