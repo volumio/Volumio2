bef06ba94744b460b30bff27d2ac64484cdc5683
Massimiliano Fanciulli <fanciulli@gmail.com>
2015-11-04 22:30:20 +0100

    Album art included in existing express instance

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ var httpServer = expressApp.listen(expressApp.get('port'), function() {
 	console.log('Express server listening on port ' + httpServer.address().port);
 });
 
+
+var albumart=require(__dirname+'/app/plugins/miscellanea/albumart/albumart.js');
+
+albumart.setFolder('/data/albumart');
+
+expressApp.get('/albumart', albumart.processExpressRequest);
+
+expressApp.use(function(err, req, res, next) {
+	/**
+	 * Replace with Winston logging
+	 **/
+	console.log('An internal error occurred while serving an albumart. Details: '+err.stack);
+
+	/**
+	 * Sending back error code 500
+	 **/
+	res.sendFile(__dirname+'/app/plugins/miscellanea/albumart/default.png');
+});
+
+
 var commandRouter = new (require('./app/index.js'))(httpServer);
