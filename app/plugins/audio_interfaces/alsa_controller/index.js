269b4f99b0213d2e53a4236c0465bb4a9b7feb1c
Conrad Winchester <conrad@chiwestern.com>
2016-01-22 13:14:28 +0000

    Changed spaces bback to tabs to keep inlione with everyone else

--- app/plugins/audio_interfaces/alsa_controller/index.js
+++ app/plugins/audio_interfaces/alsa_controller/index.js
@@ -7,99 +7,99 @@ var S = require('string');
 // Define the ControllerMpd class
 module.exports = ControllerAlsa;
 function ControllerAlsa(context) {
-    // This fixed variable will let us refer to 'this' object at deeper scopes
-    var self = this;
-    self.context = context;
-    self.commandRouter = self.context.coreCommand;
-    self.logger = self.context.logger;
+	// This fixed variable will let us refer to 'this' object at deeper scopes
+	var self = this;
+	self.context = context;
+	self.commandRouter = self.context.coreCommand;
+	self.logger = self.context.logger;
 
 }
 
 ControllerAlsa.prototype.onVolumioStart = function () {
-    var self = this;
+	var self = this;
 
-    var configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
+	var configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
 
-    self.config = new (require('v-conf'))();
-    self.config.loadFile(configFile);
+	self.config = new (require('v-conf'))();
+	self.config.loadFile(configFile);
 
 
-    var volume = self.config.get('volumestart');
+	var volume = self.config.get('volumestart');
 
-    var socketURL = 'http://localhost:3000';
-    var options = {
-        transports: ['websocket'],
-        'force new connection': true
-    };
+	var socketURL = 'http://localhost:3000';
+	var options = {
+		transports: ['websocket'],
+		'force new connection': true
+	};
 
-    var client1 = io.connect(socketURL, options);
+	var client1 = io.connect(socketURL, options);
 
-    client1.on('connect', function (data) {
-        self.logger.info("Setting volume on startup at " + volume);
-        client1.emit('volume', volume);
-    });
+	client1.on('connect', function (data) {
+		self.logger.info("Setting volume on startup at " + volume);
+		client1.emit('volume', volume);
+	});
 
-    if (self.config.has('outputdevice') == false)
-        self.config.addConfigValue('outputdevice', 'string', '0');
+	if (self.config.has('outputdevice') == false)
+		self.config.addConfigValue('outputdevice', 'string', '0');
 
-    self.logger.debug("Creating shared var alsa.outputdevice");
-    self.commandRouter.sharedVars.addConfigValue('alsa.outputdevice', 'string', self.config.get('outputdevice'));
-    self.commandRouter.sharedVars.registerCallback('alsa.outputdevice', self.outputDeviceCallback.bind(self));
+	self.logger.debug("Creating shared var alsa.outputdevice");
+	self.commandRouter.sharedVars.addConfigValue('alsa.outputdevice', 'string', self.config.get('outputdevice'));
+	self.commandRouter.sharedVars.registerCallback('alsa.outputdevice', self.outputDeviceCallback.bind(self));
 };
 
 ControllerAlsa.prototype.outputDeviceCallback = function (value) {
-    var self = this;
+	var self = this;
 
-    self.config.set('outputdevice', value);
+	self.config.set('outputdevice', value);
 };
 
 ControllerAlsa.prototype.getConfigParam = function (key) {
-    var self = this;
+	var self = this;
 
-    return self.config.get(key);
+	return self.config.get(key);
 };
 
 ControllerAlsa.prototype.setConfigParam = function (data) {
-    var self = this;
+	var self = this;
 
-    self.config.set(data.key, data.value);
+	self.config.set(data.key, data.value);
 };
 
 
 ControllerAlsa.prototype.getConfigurationFiles = function () {
-    var self = this;
+	var self = this;
 
-    return ['config.json'];
+	return ['config.json'];
 };
 
 ControllerAlsa.prototype.getAlsaCards = function () {
-    var self = this;
-    var cards = [];
+	var self = this;
+	var cards = [];
 
-    var soundCardDir = '/proc/asound/';
-    var infoFile = '/pcm0p/info';
-    var regex = /card(\d+)/;
+	var soundCardDir = '/proc/asound/';
+	var infoFile = '/pcm0p/info';
+	var regex = /card(\d+)/;
 
-    var soundFiles = fs.readdirSync(soundCardDir);
+	var soundFiles = fs.readdirSync(soundCardDir);
 
-    for (var i = 0; i < soundFiles.length; i++) {
-        var fileName = soundFiles[i];
-        var matches = regex.exec(fileName);
-        var infoFileName = soundCardDir + fileName + infoFile;
-        if (matches && fs.existsSync(infoFileName)) {
-            var index = matches[1];
-            var content = fs.readFileSync(infoFileName);
+	for (var i = 0; i < soundFiles.length; i++) {
+		var fileName = soundFiles[i];
+		var matches = regex.exec(fileName);
+		var infoFileName = soundCardDir + fileName + infoFile;
+		if (matches && fs.existsSync(infoFileName)) {
+			var index = matches[1];
+			var content = fs.readFileSync(infoFileName);
 
-            var splitted = content.toString().split('\n');
-            for (var j = 0; j < splitted.length; j++) {
-                var line = S(splitted[j]);
-                if (line.startsWith('id:')) {
-                    cards.push({id: index, name: line.chompLeft('id:').trim().s});
-                    break;
-                }
-            }
-        }
-    }
+			var splitted = content.toString().split('\n');
+			for (var j = 0; j < splitted.length; j++) {
+				var line = S(splitted[j]);
+				if (line.startsWith('id:')) {
+					cards.push({id: index, name: line.chompLeft('id:').trim().s});
+					break;
+				}
+			}
+		}
+	}
 
-    return cards;
+	return cards;
 };
