aa60a478cfa77495b66b39c4653a0e5275b66ca4
volumio <info@volumio.org>
2016-11-11 18:25:02 +0000

    added network manager to pull

--- app/plugins/system_controller/volumio_command_line_client/commands/pull.sh
+++ app/plugins/system_controller/volumio_command_line_client/commands/pull.sh
@@ -8,5 +8,8 @@ echo "Copying Modules"
 cp -rp /volumio-current/node_modules /volumio/node_modules
 echo "Copying UI"
 cp -rp /volumio-current/http/www /volumio/http/www
+echo "Getting Network Manager"
+wget https://raw.githubusercontent.com/volumio/Build/master/volumio/bin/wireless.js -O /volumio/app/plugins/system_controller/network/wireless.js
 echo "Setting Proper permissions"
 chown -R volumio:volumio /volumio
+chmod a+x /volumio/app/plugins/system_controller/network/wireless.js
