06192568fcd79473532d3e0904005cbf0d445c18
xipmix <xipmix@users.noreply.github.com>
2017-03-14 07:52:32 +1100

    More robust mounting (#1057)
    * Improve handling of paths with 'tricky' characters
    
    The existing mountShare code takes some trouble to escape any spaces in
    a NAS share name or NFS path. But this ignores other characters, such as '('.
    Under the hood the mount is done as a shell exec call to 'mount'; with such
    characters (which the shell treats specially) in the path, the mount fails.
    Related to this, NAS aliases (used to construct mountpoint paths) could also
    have characters that cause problems, such as (, " or '.
    
    To fix all this, first we have to fix linux-mountutils, see the patch below.
    This allows us to ditch the space-escaping of remote paths and ensures that
    'raw' mountpoint paths we pass to linux-mountutils are handled correctly.
    We also need to take the same care with quoting when shell execs are performed
    within this plugin, such as in getMountSize().
    
      --- node_modules/linux-mountutils/mountutils.js.orig    2017-03-06 23:39:28.132120603 +1100
      +++ node_modules/linux-mountutils/mountutils.js 2017-03-07 00:06:41.765539915 +1100
      @@ -6,6 +6,23 @@
        *
        */
    
      +/*
      + * Hide characters the shell is sensitive to inside single quotes.
      + * Shell quoting rules require this to be done by:
      + * splitting on any single quotes, wrapping each piece in single quotes
      + * and joining the pieces with escaped single quotes.
      + */
      +var quotepath = function(path) {
      +    var output = '';
      +    var pieces = path.split("'");
      +    var n = pieces.length;
      +    for (var i=0; i<n; i++) {
      +        output = output + "'" + pieces[i] + "'";
      +        if (i< (n-1)) output = output + "\\'";
      +    }
      +    return output;
      +}
      +
       /**
        * isMounted
        *
      @@ -94,6 +111,10 @@
           return;
         }
    
      +  // properly quote paths so the shell doesn't explode
      +  var qdev  = quotepath(dev);
      +  var qpath = quotepath(path);
      +
         // Build the command line
         var cmd = (options.noSudo?"":
             (options.sudoPath?options.sudoPath:"/usr/bin/sudo")+" ") +
      @@ -101,7 +122,7 @@
             (options.readonly?"-r ":"") +
             (options.fstype?"-t " + options.fstype + " ":"") +
             (options.fsopts?"-o " + options.fsopts + " ":"") +
      -      dev + " " + path;
      +      qdev + " " + qpath;
    
         // Let's do it!
         var mountProc = exec(cmd, function(error, stdout, stderr) {
      @@ -137,10 +158,11 @@
           return;
         }
    
      +  var qpath = quotepath(path);
         // Build the command line
         var cmd = (options.noSudo?"":
             (options.sudoPath?options.sudoPath:"/usr/bin/sudo")+" ") +
      -      (options.umountPath?options.umountPath:"/bin/umount") + " " + path;
      +      (options.umountPath?options.umountPath:"/bin/umount") + " " + qpath;
    
         // Let's do it!
         var umountProc = exec(cmd, function(error, stdout, stderr) {
    
    * Make sure unexpected error messages reach the caller
    
    The split on the string 'mount error' throws away some preamble text
    that clutters up the error message. But if that string does not appear
    in the error message, the code as it stands throws away the entire message.
    
    * Fix indentation

--- app/plugins/system_controller/networkfs/index.js
+++ app/plugins/system_controller/networkfs/index.js
@@ -160,10 +160,10 @@ ControllerNetworkfs.prototype.mountShare = function (data) {
 
 	var fstype = config.get('NasMounts.' + shareid + '.fstype');
 	var options = config.get('NasMounts.' + shareid + '.options');
-	var pathraw = config.get('NasMounts.' + shareid + '.path');
+	var path = config.get('NasMounts.' + shareid + '.path');
 	var mountidraw = config.get('NasMounts.' + shareid + '.name');
 	// Check we have sane data - operating on undefined values will crash us
-	if (fstype === 'undefined' || pathraw === 'undefined') {
+	if (fstype === 'undefined' || path === 'undefined') {
 		console.log('Unable to retrieve config for share '  + shareid + ', returning early');
 		return defer.promise;
 	}
@@ -171,14 +171,13 @@ ControllerNetworkfs.prototype.mountShare = function (data) {
 	var fsopts;
 	var credentials;
 	var responsemessage = {status:""};
-	var path = pathraw.replace(/ /g,"\\ ");
 	// The local mountpoint path must not contain these characters, because
 	// they get specially encoded in /etc/mtab and cause mount/umount failures.
 	// See getmntent(7).
 	var mountid = mountidraw.replace(/[\s\n\\]/g,"_");
 
 	if (fstype == "cifs") {
-		pointer = '//' + config.get('NasMounts.' + shareid + '.ip') + '/' + path.replace(' ', '\ ');;
+		pointer = '//' + config.get('NasMounts.' + shareid + '.ip') + '/' + path;
 		//Password-protected mount
 		if (config.get('NasMounts.' + shareid + '.user') !== 'undefined' && config.get('NasMounts.' + shareid + '.user') !== '') {
 			credentials = 'username=' + config.get('NasMounts.' + shareid + '.user') + ',' + 'password=' + config.get('NasMounts.' + shareid + '.password') + ",";
@@ -192,7 +191,7 @@ ControllerNetworkfs.prototype.mountShare = function (data) {
 		}
 
 	} else { // nfs
-		pointer = config.get('NasMounts.' + shareid + '.ip') + ':' + path.replace(' ', '\ ');;
+		pointer = config.get('NasMounts.' + shareid + '.ip') + ':' + path;
 		if (options) {
 			fsopts ="ro,soft,noauto,"+options;
 		} else {
@@ -209,20 +208,21 @@ ControllerNetworkfs.prototype.mountShare = function (data) {
 				result.error = 'Permission denied';
 			} else {
 				var splitreason = result.error.split('mount error');
-				result.error = splitreason[1]
+				// if the split does not match, splitreason[1] is undefined
+				if (splitreason.length > 1) result.error = splitreason[1];
 			}
 			responsemessage = {status:"fail", reason:result.error}
 			defer.resolve(responsemessage);
 			if (data.init) {
-			if (trial < 4) {
-				trial++
-				self.logger.info("Cannot mount NAS "+mountid+" at system boot, trial number "+trial+" ,retrying in 5 seconds");
-				setTimeout(function () {
-				self.mountShare({init:true, key:data.key, trial:trial});
-				}, 5000);
-			} else {
-				self.logger.info("Cannot mount NAS at system boot, trial number "+trial+" ,stopping");
-			}
+				if (trial < 4) {
+					trial++
+					self.logger.info("Cannot mount NAS "+mountid+" at system boot, trial number "+trial+" ,retrying in 5 seconds");
+					setTimeout(function () {
+						self.mountShare({init:true, key:data.key, trial:trial});
+					}, 5000);
+				} else {
+					self.logger.info("Cannot mount NAS at system boot, trial number "+trial+" ,stopping");
+				}
 			}
 		} else {
 			responsemessage = {status:"success"}
@@ -529,8 +529,9 @@ ControllerNetworkfs.prototype.getMountSize = function (share) {
 			mounted: mounted.mounted,
 			size: ''
 		};
+		var quotedmount = quotePath(mountpoint);
 		// cmd returns size in bytes with no units and no header line
-		var cmd="df -B1 --output=used '"+mountpoint+"' | tail -1";
+		var cmd="df -B1 --output=used " + quotedmount + " | tail -1";
 		var promise = libQ.ncall(exec,respShare,cmd).then(function (stdout){
 
 
@@ -556,6 +557,20 @@ ControllerNetworkfs.prototype.getMountSize = function (share) {
 		});
 	});
 };
+
+// Properly single-quote a path that will be handed to a shell exec.
+var quotePath = function (path) {
+	var self = this;
+	var output = '';
+
+	var pieces = path.split("'");
+	var n = pieces.length;
+	for (var i=0; i<n; i++) {
+		output = output + "'" + pieces[i] + "'";
+		if (i< (n-1)) output = output + "\\'";
+	}
+	return output;
+}
 
 /**
  * {
