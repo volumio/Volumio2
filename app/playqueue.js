6683402bbf8c7e66a9ded0a873ea97e4b0d84b58
Volumio <info@volumio.org>
2017-03-27 11:48:24 +0200

    Revert "Favourties fix" (#1096)
    * Revert "Update strings_fr (#1092)"
    
    This reverts commit 9af077e7705351f925ee79d1651b737d40f8f95f.
    
    * Revert "New language: czech (#1094)"
    
    This reverts commit ca0603bd463cbee364177fa26691659a0200f64d.
    
    * Revert "fix mixer control set to None"
    
    This reverts commit 0ee5830d72efb2a019e23de171f1306bf3499e26.
    
    * Revert "Queue move issue (#1090)"
    
    This reverts commit 351f5f02647f38a941417b996093c61b762c87d6.
    
    * Revert "Favourties fix (#1089)"
    
    This reverts commit 7d0b24afa2defddfc65726609071fddb9aa0a71e.

--- app/playqueue.js
+++ app/playqueue.js
@@ -194,24 +194,13 @@ CorePlayQueue.prototype.addQueueItems = function (arrayItems) {
 };
 
 CorePlayQueue.prototype.clearAddPlayQueue = function (arrayItems) {
-    var self=this;
     this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'CorePlayQueue::clearAddPlayQueue');
     this.arrayQueue = [];
     this.arrayQueue = this.arrayQueue.concat(arrayItems);
     this.saveQueue();
 
-    var defer=libQ.defer();
-
-    this.commandRouter.volumioPushQueue(this.arrayQueue)
-        .then(function(){
-            self.commandRouter.volumioPlay();
-            defer.resolve();
-        })
-        .fail(function(err)
-        {
-            defer.reject(new Error(err));
-        });
-    return defer.promise;
+    this.commandRouter.serviceClearAddPlayTracks(arrayItems,arrayItems[0].service);
+    return this.commandRouter.volumioPushQueue(this.arrayQueue);
 };
 
 CorePlayQueue.prototype.clearPlayQueue = function () {
