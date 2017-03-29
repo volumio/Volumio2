97b869dd3062bd39068b36c9632cf8a095b2b76f
Xipmix <xipmix@users.noreply.github.com>
2017-03-29 20:21:50 +1100

    Update pluginapi example

--- pluginapi/installPlugin.js
+++ pluginapi/installPlugin.js
@@ -6,7 +6,8 @@ var io=require('socket.io-client');
 var socket= io.connect('http://localhost:3000');
 
 console.log("GET BrowseLibrary\n\n");
-socket.emit('installPlugin', { url:'http://127.0.0.1:3000/plugin-serve/spotify.zip'});
+//socket.emit('installPlugin', { url:'http://127.0.0.1:3000/plugin-serve/spotify.zip'});
+socket.emit('installPlugin', { sourcefile:'/tmp/somefilewedownloaded.zip' } );
 
 socket.on('installPluginStatus',function(data)
 {
