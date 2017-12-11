9d21e038204ae5547434fc7b154a58a69fe2a013
volumio <info@volumio.org>
2017-12-11 00:01:03 +0000

    myvolumio functions

--- app/plugins/user_interface/websocket/index.js
+++ app/plugins/user_interface/websocket/index.js
@@ -1807,6 +1807,98 @@ function InterfaceWebUI(context) {
                 }
             });
 
+            connWebSocket.on('getMyVolumioStatus', function () {
+                var selfConnWebSocket = this;
+
+                var remove = self.commandRouter.getMyVolumioStatus();
+
+                if (remove != undefined) {
+                    remove.then(function (result) {
+                        selfConnWebSocket.emit('pushMyVolumioStatus', result);
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
+            connWebSocket.on('getMyVolumioToken', function (data) {
+                var selfConnWebSocket = this;
+
+                var remove = self.commandRouter.getMyVolumioToken(data);
+
+                if (remove != undefined) {
+                    remove.then(function (result) {
+                        selfConnWebSocket.emit('pushMyVolumioToken', result);
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
+            connWebSocket.on('setMyVolumioToken', function (data) {
+                var selfConnWebSocket = this;
+
+                var token = self.commandRouter.setMyVolumioToken(data);
+
+                if (token != undefined) {
+                    token.then(function (result) {
+                        self.commandRouter.broadcastMessage('pushMyVolumioToken', {"token":result});
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
+            connWebSocket.on('myVolumioLogout', function () {
+                var selfConnWebSocket = this;
+
+                self.commandRouter.broadcastMessage('pushMyVolumioLogout', '');
+
+                return self.commandRouter.myVolumioLogout();
+            });
+
+            connWebSocket.on('enableMyVolumioDevice', function (device) {
+                var selfConnWebSocket = this;
+
+                var enable = self.commandRouter.enableMyVolumioDevice(device);
+
+                if (enable != undefined) {
+                    enable.then(function (result) {
+                        //selfConnWebSocket.emit('pushMyVolumioStatus', result);
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
+            connWebSocket.on('disableMyVolumioDevice', function (device) {
+                var selfConnWebSocket = this;
+
+                var disable = self.commandRouter.disableMyVolumioDevice(device);
+
+                if (disable != undefined) {
+                    disable.then(function (result) {
+                        //selfConnWebSocket.emit('pushMyVolumioStatus', result);
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
+            connWebSocket.on('deleteMyVolumioDevice', function (device) {
+                var selfConnWebSocket = this;
+
+                var deleteDevice = self.commandRouter.deleteMyVolumioDevice(device);
+
+                if (deleteDevice != undefined) {
+                    deleteDevice.then(function (result) {
+                        //selfConnWebSocket.emit('pushMyVolumioStatus', result);
+                    })
+                        .fail(function () {
+                        });
+                }
+            });
+
 		}
 		catch (ex) {
 			self.logger.error("Catched an error in socketio. Details: " + ex);
