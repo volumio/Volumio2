fd6ef5003529eaf6e999c02352afa8f990af3774
Volumio <info@volumio.org>
2018-04-12 19:03:38 +0000

    Pack modules util script

--- utils/misc/clean-node-modules.js
+++ utils/misc/clean-node-modules.js
@@ -1,6 +1,12 @@
 const modclean = require('modclean');
 const execSync = require('child_process').execSync;
-const modulesFolder = process.cwd() + '/node_modules';
+var modulesFolder = modulesFolder = process.cwd() + '/node_modules';
+var  folder = process.cwd();
+
+if (process.argv[2]) {
+    modulesFolder = process.argv[2] + '/node_modules';
+    folder = process.argv[2];
+}
 
 console.log('Cleaning node modules in folder ' + modulesFolder);
 
@@ -11,7 +17,7 @@ try {
     console.log('Error, cannot determine size for ' + modulesFolder);
 }
 
-modclean(function(err, results) {
+modclean({"cwd":folder}, function(err, results) {
     if(err) {
         console.error('Error, cannot clean: ' + err);
         return;
---
+++ utils/misc/pack-modules.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+NODE_VERSION=`node --version | tr -d v`
+ARCH=`/bin/cat /etc/os-release | grep "VOLUMIO_ARCH" | cut -d \\" -f2 | tr -d "\n"`
+NODE_MODULES_PACKAGE=/volumio/node_modules_${ARCH}-${NODE_VERSION}.tar.gz
+
+if [ -f $NODE_MODULES_PACKAGE ]; then
+   echo "Cleaning previous package file"
+   rm $NODE_MODULES_PACKAGE
+fi
+
+cd /volumio
+
+echo "Cleaning modules"
+node /volumio/utils/misc/clean-node-modules.js /volumio
+
+echo "Packing modules..."
+tar zcf $NODE_MODULES_PACKAGE node_modules/*
+NODE_MODULES_PACKAGE_SIZE=`du -sbm ${NODE_MODULES_PACKAGE} | cut -f1`
+echo "Done, package size ${NODE_MODULES_PACKAGE_SIZE} MB"
