0c446b0fb21e65844b4369b059c2c94d7ed3e5b3
Volumio <info@volumio.org>
2018-05-24 23:42:52 +0200

    Create devmode file when volumio dev is launched

--- app/plugins/system_controller/volumio_command_line_client/commands/devmode.sh
+++ app/plugins/system_controller/volumio_command_line_client/commands/devmode.sh
@@ -3,6 +3,7 @@
 NODEMON_DIR=/volumio/node_modules/nodemon/
 ETH_IP=`/usr/bin/sudo /sbin/ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
 PORT=9229
+DEV_FILE=/data/devmode
 
 echo "Stopping Volumio service"
 /usr/bin/sudo /bin/systemctl stop volumio
@@ -14,6 +15,9 @@ if [ ! -d "$NODEMON_DIR" ]; then
   /bin/npm install nodemon
 fi
 
+if [ ! -f "$DEV_FILE" ]; then
+   touch $DEV_FILE
+fi
 
 echo 'Starting Volumio with debugger at' $ETH_IP:$PORT
 cd /volumio
