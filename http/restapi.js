211e5bc46fc5140f978baf5fee775a5a3a15cfa7
Volumio <info@volumio.org>
2018-09-14 19:16:41 +0000

    Network parsing for jessie and stretch

--- http/restapi.js
+++ http/restapi.js
@@ -3,7 +3,7 @@ var app = require('./index.js')
 var bodyParser = require('body-parser');
 var ip = require('ip');
 var api = express.Router();
-var ifconfig = require('wireless-tools/ifconfig');
+var ifconfig = require('/volumio/app/plugins/system_controller/network/lib/ifconfig.js');
 var primaryhost = undefined;
 
 function apiInterface(server, commandRouter) {
