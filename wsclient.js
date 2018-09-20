01b5fe34a75e315803f7754605ac102d8defc6c6
Ash <ashthespy@users.noreply.github.com>
2018-09-20 20:54:46 +0200

    Set up consistant codestyle (#1613)
    * [codestyle] Set up git hooks workflow for code formatting
    
    * [codestyle] Test new styleguide (formatting)

--- wsclient.js
+++ wsclient.js
@@ -1,10 +1,10 @@
-var io=require('socket.io-client');
+var io = require('socket.io-client');
 
-var socket= io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:3000');
 
 var event = process.argv[2];
 var message = process.argv[3];
 
-console.log("EVENT: "+event);
-console.log("MESSAGE: "+message);
+console.log('EVENT: ' + event);
+console.log('MESSAGE: ' + message);
 socket.emit(event, message);
