19faa24bdf06642eed0e8f629b76a08c950a8a85
ashthespy <ashthespy@gmail.com>
2018-09-20 03:02:03 +0200

    [codestyle] Test new styleguide (formatting)

--- logsubmit.js
+++ logsubmit.js
@@ -3,13 +3,13 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 
 var commandArray = [
-    "cat /proc/version",
-    "cat /etc/os-release",
-    "ifconfig",
-    "iwconfig",
-    "aplay -l",
-    "ps -ef",
-    "sudo journalctl -p 7"
+  'cat /proc/version',
+  'cat /etc/os-release',
+  'ifconfig',
+  'iwconfig',
+  'aplay -l',
+  'ps -ef',
+  'sudo journalctl -p 7'
 ];
 
 // Up to two arguments may be given:
@@ -17,109 +17,108 @@ var commandArray = [
 // - an optional flag indicating the log should be saved locally, not uploaded
 var args = process.argv.slice(2);
 var description;
-if ( args[0] == undefined ) {
-    description = 'Unknown';
+if (args[0] == undefined) {
+  description = 'Unknown';
 } else {
-    description = '';
-    // This will always yield a string that starts and ends with single quotes.
-    var pieces = args[0].split("'");
-    var n = pieces.length;
-    for (var i=0; i<n; i++) {
-        description = description + "'" + pieces[i] + "'";
-        if (i < (n-1)) description = description + "\\'";
-    }
+  description = '';
+  // This will always yield a string that starts and ends with single quotes.
+  var pieces = args[0].split("'");
+  var n = pieces.length;
+  for (var i = 0; i < n; i++) {
+    description = description + "'" + pieces[i] + "'";
+    if (i < (n - 1)) description = description + "\\'";
+  }
 }
 
 var submit = 'yes';
-if ( args.length > 1 ) {
-    submit = 'no';
+if (args.length > 1) {
+  submit = 'no';
 }
 
-var logFile = "/tmp/logondemand";
-var storedLogFile = "/var/tmp/logondemand";
+var logFile = '/tmp/logondemand';
+var storedLogFile = '/var/tmp/logondemand';
 
 // Let's start fresh!
-execSync("date >" + logFile);
+execSync('date >' + logFile);
 
 try {
-    //If description is supplied, add it
-    fs.appendFileSync(logFile, 'Description="' + description + '"\n');
+  // If description is supplied, add it
+  fs.appendFileSync(logFile, 'Description="' + description + '"\n');
 } catch (e) {
-    console.log(e);
+  console.log(e);
 }
 
 for (var itemN in commandArray) {
-    var item = commandArray[itemN];
-    var itemWithoutput = item + " >>" + logFile + " 2>&1"
-    try {
-        fs.appendFileSync(logFile, '\n# ' + item + ' ---------------' + '\n');
-    } catch(e) {
-        console.log(e);
-    }
-    execSync(itemWithoutput);
+  var item = commandArray[itemN];
+  var itemWithoutput = item + ' >>' + logFile + ' 2>&1';
+  try {
+    fs.appendFileSync(logFile, '\n# ' + item + ' ---------------' + '\n');
+  } catch (e) {
+    console.log(e);
+  }
+  execSync(itemWithoutput);
 }
 
 // remove sensitive information
 commandArray = [
-    "sed -i -r -e 's/([Pp]assword:  *)([^ ]*)(.*)$/\\1<elided> \\3/'",
-    "sed -i -r -e 's/([Ss]potify  *.*token is )(.*)$/\\1<elided>/'",
-    "sed -i -r -e 's/(--[Pp]assword[ ][ ]*)([^ ]*)/\\1<elided>/'",
-    "sed -i -r -e 's/(wlan[0-9]: WPS: UUID [^:]*: ).*$/\\1<elided>/'",
-    "sed -i -r -e 's/(mount .*username=)([^,]*,)(.*)$/\\1<elided>,\\3/'",
-    "sed -i -r -e 's/(mount .*password=)([^,]*,)(.*)$/\\1<elided>,\\3/'"
+  "sed -i -r -e 's/([Pp]assword:  *)([^ ]*)(.*)$/\\1<elided> \\3/'",
+  "sed -i -r -e 's/([Ss]potify  *.*token is )(.*)$/\\1<elided>/'",
+  "sed -i -r -e 's/(--[Pp]assword[ ][ ]*)([^ ]*)/\\1<elided>/'",
+  "sed -i -r -e 's/(wlan[0-9]: WPS: UUID [^:]*: ).*$/\\1<elided>/'",
+  "sed -i -r -e 's/(mount .*username=)([^,]*,)(.*)$/\\1<elided>,\\3/'",
+  "sed -i -r -e 's/(mount .*password=)([^,]*,)(.*)$/\\1<elided>,\\3/'"
 ];
 for (var itemN in commandArray) {
-	var item = commandArray[itemN];
-	var cmd  = item + " " + logFile;
-	try {
-		execSync(cmd);
-	} catch(e) {
-		console.log(e);
-	}
+  var item = commandArray[itemN];
+  var cmd = item + ' ' + logFile;
+  try {
+    execSync(cmd);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 var variant = getSystemVersion();
 
 // Use single quotes to avoid the shell expanding any characters in the form data
 // description is a special case, see above
-var command = "/usr/bin/curl -X POST -H 'Content-Type: multipart/form-data'"
-            + " -F 'logFile=@" + logFile + "'"
-            + " -F desc=" + description
-            + " -F 'variant=" + variant + "'"
-            + " 'http://logs.volumio.org:7171/logs/v1'";
+var command = "/usr/bin/curl -X POST -H 'Content-Type: multipart/form-data'" +
+            " -F 'logFile=@" + logFile + "'" +
+            ' -F desc=' + description +
+            " -F 'variant=" + variant + "'" +
+            " 'http://logs.volumio.org:7171/logs/v1'";
 
-if ( submit === 'yes' ) {
-exec(command , {uid: 1000, gid: 1000, encoding: 'utf8'}, function (error, stdout, stderr) {
+if (submit === 'yes') {
+  exec(command, {uid: 1000, gid: 1000, encoding: 'utf8'}, function (error, stdout, stderr) {
     if (error !== null) {
-        console.log('Cannot send bug report: ' + error);
-        console.log('Saving as: ' + storedLogFile);
-        execSync("mv -f " + logFile + " " + storedLogFile);
+      console.log('Cannot send bug report: ' + error);
+      console.log('Saving as: ' + storedLogFile);
+      execSync('mv -f ' + logFile + ' ' + storedLogFile);
     } else {
-        console.log(stdout)
-        execSync("rm " + logFile);
+      console.log(stdout);
+      execSync('rm ' + logFile);
     }
-});
+  });
 } else {
-    console.log('Saving as: ' + storedLogFile);
-    execSync("mv -f " + logFile + " " + storedLogFile);
+  console.log('Saving as: ' + storedLogFile);
+  execSync('mv -f ' + logFile + ' ' + storedLogFile);
 }
 
 function randomIntInc (low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
+  return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
 function getSystemVersion () {
-    var self = this;
-    var file = fs.readFileSync('/etc/os-release').toString().split('\n');
+  var self = this;
+  var file = fs.readFileSync('/etc/os-release').toString().split('\n');
 
-    var nLines = file.length;
-    var str;
-    for (var l = 0; l < nLines; l++) {
-
-        if (file[l].match(/VOLUMIO_VARIANT/i)) {
-            str = file[l].split('=');
-            var variant = str[1].replace(/\"/gi, "");
-            return variant;
-        }
+  var nLines = file.length;
+  var str;
+  for (var l = 0; l < nLines; l++) {
+    if (file[l].match(/VOLUMIO_VARIANT/i)) {
+      str = file[l].split('=');
+      var variant = str[1].replace(/\"/gi, '');
+      return variant;
     }
-};
+  }
+}
--- update-helper.js
+++ update-helper.js
@@ -6,149 +6,148 @@ var inquirer = require('inquirer');
 
 var errorMessage = 'Error, please provide the required updater action: forceupdate | factory | userdata | testmode | cleanupdate | restorevolumio';
 if (process.argv[2]) {
-    var argument = process.argv[2];
+  var argument = process.argv[2];
 
-    switch (argument){
-        case "forceupdate":
-            forceUpdate()
-            break;
-        case "factory":
-            factoryReset()
-            break;
-        case "userdata":
-            deleteUserData()
-            break;
-        case "testmode":
-            testMode()
-            break;
-        case "cleanupdate":
-            cleanUpdate()
-            break;
-        case "restorevolumio":
-            restoreVolumio()
-            break;
-        default:
-            console.log(errorMessage);
-    }
+  switch (argument) {
+    case 'forceupdate':
+      forceUpdate();
+      break;
+    case 'factory':
+      factoryReset();
+      break;
+    case 'userdata':
+      deleteUserData();
+      break;
+    case 'testmode':
+      testMode();
+      break;
+    case 'cleanupdate':
+      cleanUpdate();
+      break;
+    case 'restorevolumio':
+      restoreVolumio();
+      break;
+    default:
+      console.log(errorMessage);
+  }
 } else {
-    console.log(errorMessage)
+  console.log(errorMessage);
 }
 
-function forceUpdate(data) {
-    var clean = false;
-    var socket = io.connect('http://localhost:3000');
+function forceUpdate (data) {
+  var clean = false;
+  var socket = io.connect('http://localhost:3000');
 
-    console.log('Checking for new Updates');
-    socket.emit('updateCheck');
+  console.log('Checking for new Updates');
+  socket.emit('updateCheck');
 
-    if (data && data.clean) {
-        clean = true;
-    }
+  if (data && data.clean) {
+    clean = true;
+  }
 
-    socket.on('updateReady', function(data) {
-        if (data.updateavailable) {
-            var question = [
-                {
-                    type: 'confirm',
-                    name: 'executeUpdate',
-                    message: data.title + ' is available. Do you want to Update?'
-                }
-            ];
-            inquirer.prompt(question).then((answer) => {
-                if (answer.executeUpdate) {
-                    if (clean) {
-                        console.log('Executing a clean update');
-                        execSync('/bin/touch /boot/user_data', {uid: 1000, gid: 1000});
-                    }
-                    executeUpdate(socket);
-                } else {
-                    console.log('Exiting...');
-                    return process.exit(0);
-            }
-            })
+  socket.on('updateReady', function (data) {
+    if (data.updateavailable) {
+      var question = [
+        {
+          type: 'confirm',
+          name: 'executeUpdate',
+          message: data.title + ' is available. Do you want to Update?'
+        }
+      ];
+      inquirer.prompt(question).then((answer) => {
+        if (answer.executeUpdate) {
+          if (clean) {
+            console.log('Executing a clean update');
+            execSync('/bin/touch /boot/user_data', {uid: 1000, gid: 1000});
+          }
+          executeUpdate(socket);
         } else {
-            console.log('No update available');
-            return process.exit(0);
+          console.log('Exiting...');
+          return process.exit(0);
         }
-        //
-    });
-}
-
-function cleanUpdate() {
-    forceUpdate({"clean":true});
-}
-
-function executeUpdate(socket) {
-    console.log('Starting Update...');
-    var payload = {'ignoreIntegrityCheck':true};
-    socket.emit('update', payload);
-
-    socket.on('updateProgress', function(data) {
-        if (data.progress && data.status) {
-            console.log('Updating: ' + data.progress + '% : ' + data.status);
-        }
-    });
-
-    socket.on('updateDone', function(data) {
-        if (data.status === 'Error') {
-            console.log('Cannot complete update, error')
-        } else {
-            console.log('Update completed successfully, restarting');
-            return reboot(socket)
-        }
-    });
-}
-
-
-function factoryReset() {
-    console.log('Factory reset initiated, the system will restart now.')
-    execSync('/bin/touch /boot/factory_reset', {uid: 1000, gid: 1000});
-    return reboot()
-}
-
-function deleteUserData() {
-    console.log('User data reset initiated, the system will restart now.')
-    execSync('/bin/touch /boot/user_data', {uid: 1000, gid: 1000});
-    return reboot()
-}
-
-function testMode() {
-    try {
-        fs.statSync('/data/test');
-        execSync('/bin/rm /data/test', {uid: 1000, gid: 1000});
-        console.log('Test mode DISABLED');
-    } catch (e) {
-        execSync('/bin/touch /data/test', {uid: 1000, gid: 1000});
-        console.log('Test mode ENABLED');
+      });
+    } else {
+      console.log('No update available');
+      return process.exit(0);
     }
+    //
+  });
 }
 
-function reboot(socket) {
-    console.log('Restarting');
-    if (socket) {
-        socket.emit('closeModals');
+function cleanUpdate () {
+  forceUpdate({'clean': true});
+}
+
+function executeUpdate (socket) {
+  console.log('Starting Update...');
+  var payload = {'ignoreIntegrityCheck': true};
+  socket.emit('update', payload);
+
+  socket.on('updateProgress', function (data) {
+    if (data.progress && data.status) {
+      console.log('Updating: ' + data.progress + '% : ' + data.status);
     }
+  });
+
+  socket.on('updateDone', function (data) {
+    if (data.status === 'Error') {
+      console.log('Cannot complete update, error');
+    } else {
+      console.log('Update completed successfully, restarting');
+      return reboot(socket);
+    }
+  });
+}
+
+function factoryReset () {
+  console.log('Factory reset initiated, the system will restart now.');
+  execSync('/bin/touch /boot/factory_reset', {uid: 1000, gid: 1000});
+  return reboot();
+}
+
+function deleteUserData () {
+  console.log('User data reset initiated, the system will restart now.');
+  execSync('/bin/touch /boot/user_data', {uid: 1000, gid: 1000});
+  return reboot();
+}
+
+function testMode () {
+  try {
+    fs.statSync('/data/test');
+    execSync('/bin/rm /data/test', {uid: 1000, gid: 1000});
+    console.log('Test mode DISABLED');
+  } catch (e) {
+    execSync('/bin/touch /data/test', {uid: 1000, gid: 1000});
+    console.log('Test mode ENABLED');
+  }
+}
+
+function reboot (socket) {
+  console.log('Restarting');
+  if (socket) {
+    socket.emit('closeModals');
+  }
+  execSync('/bin/sync', {uid: 1000, gid: 1000});
+  execSync('/usr/bin/sudo /sbin/reboot', {uid: 1000, gid: 1000});
+}
+
+function restoreVolumio () {
+  console.log('Deleting Overlay content of /volumio folder');
+  try {
+    console.log('Mounting overlay partition');
+    if (!fs.existsSync('/mnt/overlay')) {
+      execSync('/bin/mkdir /mnt/overlay', {uid: 1000, gid: 1000});
+    }
+    execSync('/usr/bin/sudo /bin/mount /dev/mmcblk0p3 /mnt/overlay', {uid: 1000, gid: 1000});
+    console.log('Overlay partition mounted, deleting /volumio folder on overlay');
+    execSync('/bin/rm -rf /mnt/overlay/dyn/volumio', {uid: 1000, gid: 1000});
+    console.log('Cleaning environment');
+    execSync('/usr/bin/sudo /bin/umount /mnt/overlay', {uid: 1000, gid: 1000});
+    execSync('/bin/rm -rf /mnt/overlay', {uid: 1000, gid: 1000});
     execSync('/bin/sync', {uid: 1000, gid: 1000});
-    execSync('/usr/bin/sudo /sbin/reboot', {uid: 1000, gid: 1000});
-}
-
-function restoreVolumio() {
-    console.log('Deleting Overlay content of /volumio folder');
-    try {
-        console.log('Mounting overlay partition');
-        if (!fs.existsSync('/mnt/overlay')) {
-            execSync('/bin/mkdir /mnt/overlay', {uid: 1000, gid: 1000});
-        }
-        execSync('/usr/bin/sudo /bin/mount /dev/mmcblk0p3 /mnt/overlay', {uid: 1000, gid: 1000});
-        console.log('Overlay partition mounted, deleting /volumio folder on overlay');
-        execSync('/bin/rm -rf /mnt/overlay/dyn/volumio', {uid: 1000, gid: 1000});
-        console.log('Cleaning environment');
-        execSync('/usr/bin/sudo /bin/umount /mnt/overlay', {uid: 1000, gid: 1000});
-        execSync('/bin/rm -rf /mnt/overlay', {uid: 1000, gid: 1000});
-        execSync('/bin/sync', {uid: 1000, gid: 1000});
-        console.log('Done, restarting system');
-        return reboot()
-    } catch (e) {
-        console.log('Could not restore /volumio folder: ' + e);
-    }
+    console.log('Done, restarting system');
+    return reboot();
+  } catch (e) {
+    console.log('Could not restore /volumio folder: ' + e);
+  }
 }
