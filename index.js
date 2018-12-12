79a394f75d2182e7c2c9801d6a531bf794f8d2e4
Volumio <info@volumio.org>
2018-12-12 18:03:13 +0000

    Fix env variable invocation

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ process.on('uncaughtException', (error) => {
     var errorMessage = 'Unknown';
   }
   execSync('/usr/local/bin/node /volumio/crashreport.js "' + errorMessage + '"');
-  if (process.env.EXIT_ON_EXCEPTION) {
+  if (process.env.EXIT_ON_EXCEPTION === "true") {
     process.exit(1);
   }
 });
