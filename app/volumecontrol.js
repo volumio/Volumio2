22f5e101174ee7044cd4597c2e9abdfd2750f4fe
Volumio <info@volumio.org>
2019-01-16 09:31:40 +0100

    Removed unused var when parsing error

--- app/volumecontrol.js
+++ app/volumecontrol.js
@@ -71,9 +71,8 @@ function CoreVolumeController(commandRouter) {
 
 		p.stderr.on('data', function (data) {
             try {
-                var parsedError = data.toString().replace(/\n/g, '');
                 // Avoid to pass pulse introduced error in parsing
-                //console.log('---' + parsedError + '---');
+                //console.log('---' + data.toString().replace(/\n/g, '') + '---');
                 if (data.toString().replace(/\n/g, '') === 'No protocol specified' || data.toString().replace(/\n/g, '') === 'xcb_connection_has_error() returned true') {
                     // ignoring those errors
                     //console.log('IGNORING')
