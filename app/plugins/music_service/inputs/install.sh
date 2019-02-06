edd260d2db8bbd48ac9e185264ae737bd014050b
ghembs <ggambacorta88@gmail.com>
2019-02-06 16:11:35 +0000

    started input plugin

---
+++ app/plugins/music_service/inputs/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing inputs Dependencies"
+sudo apt-get update
+# Install the required packages via apt-get
+sudo apt-get -y install
+
+# If you need to differentiate install for armhf and i386 you can get the variable like this
+#DPKG_ARCH=`dpkg --print-architecture`
+# Then use it to differentiate your install
+
+#requred to end the plugin install
+echo "plugininstallend"
