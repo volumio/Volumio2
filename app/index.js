781cf1cb89cda7b79aa65b5a18a5647dd2110709
gilphilbert <gil.philbert@gmail.com>
2019-10-28 08:09:30 -0700

    fix second if statement

--- app/index.js
+++ app/index.js
@@ -485,7 +485,7 @@ CoreCommandRouter.prototype.replaceAndPlay = function (data) {
                 this.volumioPlay(data.index);
         		defer.resolve();
             });
-    } else if ((!(data.list && data.index) && data.item && data.item.uri)) {
+    } else if ((!(data.list!=undefined && data.index!=undefined) && data.item!=undefined && data.item.uri!=undefined)) {
         this.stateMachine.addQueueItems(data.item)
             .then((e)=>{
                 this.volumioPlay(e.firstItemIndex);
