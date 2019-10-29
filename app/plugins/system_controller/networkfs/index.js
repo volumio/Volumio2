17c958b922f7916e6977e714af2e595317378fa3
Xipmix <xipmix@users.noreply.github.com>
2019-10-29 19:42:25 +1100

    Remove duplicate declarations

--- app/plugins/system_controller/networkfs/index.js
+++ app/plugins/system_controller/networkfs/index.js
@@ -79,7 +79,6 @@ ControllerNetworkfs.prototype.onUninstall = function () {
 ControllerNetworkfs.prototype.getUIConfig = function () {
     var self = this;
 
-    var self = this;
     var lang_code = self.commandRouter.sharedVars.get('language_code');
 
     var defer=libQ.defer();
