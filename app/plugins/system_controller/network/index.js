5ab45bdc515408d78cfaa407d143340e2d89622c
Volumio <info@volumio.org>
2020-03-12 16:11:44 +0000

    Remove console logs

--- app/plugins/system_controller/network/index.js
+++ app/plugins/system_controller/network/index.js
@@ -597,7 +597,7 @@ ControllerNetwork.prototype.rebuildHotspotConfig = function () {
 		fs.accessSync(hostapdedimax, fs.F_OK);
 		exec("/usr/bin/sudo /bin/chmod 777 "+ hostapdedimax, {uid: 1000, gid: 1000}, function (error, stdout, stderr) {
 			if (error !== null) {
-				console.log('Cannot set permissions for /etc/hostapd/hostapd-edimax.conf: ' + error);
+				self.logger.error('Cannot set permissions for /etc/hostapd/hostapd-edimax.conf: ' + error);
 
 			} else {
 				self.logger.info('Permissions for /etc/hostapd/hostapd-edimax.conf')
@@ -637,7 +637,7 @@ ControllerNetwork.prototype.rebuildHotspotConfig = function () {
 
 	exec("/usr/bin/sudo /bin/chmod 777 " + hostapd, {uid: 1000, gid: 1000}, function (error, stdout, stderr) {
 		if (error !== null) {
-			console.log('Cannot set permissions for /etc/hostapd/hostapd.conf: ' + error);
+			self.logger.error('Cannot set permissions for /etc/hostapd/hostapd.conf: ' + error);
 
 		} else {
 			self.logger.info('Permissions for /etc/hostapd/hostapd.conf')
@@ -751,7 +751,7 @@ ControllerNetwork.prototype.rebuildNetworkConfig = function () {
 
 	exec("/usr/bin/sudo /bin/chmod 777 /etc/network/interfaces && /usr/bin/sudo /bin/chmod 777 /etc/dhcpcd.conf", {uid: 1000, gid: 1000}, function (error, stdout, stderr) {
 		if (error !== null) {
-			console.log('Cannot set permissions for /etc/network/interfaces: ' + error);
+			self.logger.error('Cannot set permissions for /etc/network/interfaces: ' + error);
 
 		} else {
 			self.logger.info('Permissions for /etc/network/interfaces set')
