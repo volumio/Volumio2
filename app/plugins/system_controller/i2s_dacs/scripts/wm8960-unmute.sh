80cd6a8c21d369b3c4e1738c9b58b2e7230ae6be
Halley Tsai <hftsai256@gmail.com>
2021-07-13 09:26:49 -0400

    add wm8960 dac descriptor

---
+++ app/plugins/system_controller/i2s_dacs/scripts/wm8960-unmute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CARD=wm8960soundcard
+
+function level {
+	for s in "${@:2}"; do
+		amixer -c $CARD cset iface=MIXER,name="${s}" $1
+	done
+}
+
+level on  "Right Output Mixer PCM Playback Switch" \
+          "Left Output Mixer PCM Playback Switch"
+level 255 "Playback Volume"
+level 109 "Headphone Playback Volume" "Speaker Playback Volume"
+level 4   "Speaker DC Volume" "Speaker AC Volume"
+
+alsactl store
+
